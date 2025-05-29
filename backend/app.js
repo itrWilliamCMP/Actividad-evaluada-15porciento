129ca51183409265e3741977b3dd4a0651a0a7b2
William Campos <20230079@ricaldone.edu.sv>
2025-05-29 15:35:37 -0600

    a

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "http:https://actividad-evaluada-15porciento.vercel.app",
+    origin: "https://actividad-evaluada-15porciento.vercel.app",
     credentials: true,
   })
 );
